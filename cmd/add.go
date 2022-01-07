9dd96348bbaca79192106554ded0b53447998034
Conor Molloy <from.conor.molloy@gmail.com>
2022-01-07 16:33:10 +0000

    Plum CreateTask() in the add CLI command

--- cmd/add.go
+++ cmd/add.go
@@ -2,7 +2,9 @@ package cmd
 
 import (
 	"fmt"
+	"os"
 	"strings"
+	"task/db"
 
 	"github.com/spf13/cobra"
 )
@@ -12,6 +14,11 @@ var addCommand = &cobra.Command{
 	Short: "adds a task to your task manager",
 	Run: func(cmd *cobra.Command, args []string) {
 		task := strings.Join(args, " ")
+		_, err := db.CreateTask(task)
+		if err != nil {
+			fmt.Println("Could not create task:", err.Error())
+			os.Exit(1)
+		}
 		fmt.Printf("Added: \"%s\"\n", task)
 	},
 }
