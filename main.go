03da874ea76a4c9b5d801d3ec203feca24579f1a
Conor Molloy <from.conor.molloy@gmail.com>
2022-01-07 14:25:27 +0000

    Have the db initialezed when the application is called and small refactor to the db code

--- main.go
+++ main.go
@@ -1,7 +1,26 @@
 package main
 
-import "task/cmd"
+import (
+	"fmt"
+	"os"
+	"path/filepath"
+	"task/cmd"
+	"task/db"
+
+	"github.com/mitchellh/go-homedir"
+)
 
 func main() {
+	home, err := homedir.Dir()
+	if err != nil {
+		fmt.Printf("Failed to find the home directary")
+		os.Exit(1)
+	}
+	dbHome := filepath.Join(home, "tasks.db")
+	err = db.Init(dbHome)
+	if err != nil {
+		fmt.Printf("Filed to initialise the db")
+		os.Exit(1)
+	}
 	cmd.RootCmd.Execute()
 }
